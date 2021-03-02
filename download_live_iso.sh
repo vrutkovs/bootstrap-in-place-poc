e94f70f90f201e2db80dfbc19a78335524c28c2d
Vadim Rutkovsky <vrutkovs@redhat.com>
2021-03-02 11:16:32 +0100

    download_live_iso.sh: fetch FCOS

--- download_live_iso.sh
+++ download_live_iso.sh
@@ -1,5 +1,5 @@
-export RHCOS_VERSION=${RHCOS_VERSION:-47.83.202102090044-0}
-export BASE_OS_IMAGE=${BASE_OS_IMAGE:-https://releases-art-rhcos.svc.ci.openshift.org/art/storage/releases/rhcos-4.7/${RHCOS_VERSION}/x86_64/rhcos-${RHCOS_VERSION}-live.x86_64.iso}
+export FCOS_VERSION=${FCOS_VERSION:-33.20210201.3.0}
+export BASE_OS_IMAGE=https://builds.coreos.fedoraproject.org/prod/streams/stable/builds/${FCOS_VERSION}/x86_64/fedora-coreos-${FCOS_VERSION}-live.x86_64.iso
 
 if [ $# -eq 0 ]; then
     echo "USAGE: $0 download_path"
@@ -8,4 +8,3 @@ fi
 
 DOWNLOAD_PATH=$1
 curl ${BASE_OS_IMAGE} --retry 5 -o $DOWNLOAD_PATH
-
